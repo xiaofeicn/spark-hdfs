50f3714c63e22b250bfb93892f502babe0bd8d11
xf001 <271986756@qq.com>
2019-04-02 16:39:08 +0800

    ui port 绑定

--- src/main/scala/com/cj/spark/streaming/streaming/MQTest.scala
+++ src/main/scala/com/cj/spark/streaming/streaming/MQTest.scala
@@ -23,33 +23,34 @@ object MQTest {
       .config("spark.ui.port",spark_ui_port)
       .getOrCreate()
     val sc: SparkContext = spark.sparkContext
-    val rdd=sc.textFile("hdfs://cj-data1:8020/cj-test/original-data/2019-03-28*")
+    val rdd=sc.textFile("hdfs://cj-data1:8020/cj-test/original-data/2019-03-28/20190328_868c69f4479242f8bb377fe15352153f.json")
     println(rdd.count())
-//    val json_name_dataARR = rdd.map(x => parseObject(x)).map(x => x.getJSONArray("RecordList").toArray().map(x => parseObject(x.toString)))
-//      .flatMap(x =>
-//        x.map(s => (s.getString("fStr_TableName"), s.getJSONArray("RecordList").toArray()))
-//      ).cache()
-//    val personal_development = json_name_dataARR.filter(_._1 == "tb_tech_personal_development")
-//      .flatMap(x => x._2)
-//      .map(line => parseObject(line.toString)).map(json => (
-//      json.getString("fStr_CourseID"),
-//      json.getString("fStr_StudentID"),
-//      json.getString("fStr_SubjectName"),
-//      json.getString("fStr_Json"),
-//      json.getString("fDtt_CreateTime"),
-//      json.getString("fDtt_ModifyTime")
-//    )).filter(!_._1.equals("3496ef4b129447d5beaf237f294f669f")).filter(!_._1.equals("4fefcbc42e6b462f8dc301ca47b2f07f"))
-//
-//    import spark.implicits._
-//    val prop = getPropByEnv("test")
-//    val url="jdbc:mysql://rm-wz95c58i9ixyqw206o.mysql.rds.aliyuncs.com:3306/db_online_log?useSSL=false"
-//    personal_development.map(line => tb_personal_development(
-//      line._1,
-//      line._2,
-//      line._3,
-//      line._4,
-//      line._5,
-//      line._6)).toDF().write.mode("append").jdbc(url, "tb_personal_development", prop)
+    val json_name_dataARR = rdd.map(x => parseObject(x)).map(x => x.getJSONArray("RecordList").toArray().map(x => parseObject(x.toString)))
+      .flatMap(x =>
+        x.map(s => (s.getString("fStr_TableName"), s.getJSONArray("RecordList").toArray()))
+      ).cache()
+    val personal_development = json_name_dataARR.filter(_._1 == "tb_tech_personal_development")
+      .flatMap(x => x._2)
+      .map(line => parseObject(line.toString)).map(json => (
+      json.getString("fStr_CourseID"),
+      json.getString("fStr_StudentID"),
+      json.getString("fStr_SubjectName"),
+      json.getString("fStr_Json"),
+      json.getString("fDtt_CreateTime"),
+      json.getString("fDtt_ModifyTime")
+    ))
+//      .filter(!_._1.equals("3496ef4b129447d5beaf237f294f669f")).filter(!_._1.equals("4fefcbc42e6b462f8dc301ca47b2f07f"))
+
+    import spark.implicits._
+    val prop = getPropByEnv("test")
+    val url="jdbc:mysql://rm-wz95c58i9ixyqw206o.mysql.rds.aliyuncs.com:3306/db_online_log?useSSL=false"
+    personal_development.map(line => tb_personal_development(
+      line._1,
+      line._2,
+      line._3,
+      line._4,
+      line._5,
+      line._6)).toDF().write.mode("append").jdbc(url, "tb_personal_development", prop)
 
 
 
